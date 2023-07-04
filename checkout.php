11164dafab9bd02f4e2cb29b73b9419d4fe34929
kingi001 <kingikhamis518@gmail.com>
2023-07-04 12:43:51 +0300

    mpesa PA

--- checkout.php
+++ checkout.php
@@ -344,18 +344,7 @@ if(!isset($_SESSION['cart_p_id'])) {
                                         </div>
                                     </form>
 
-                                    <form class="mpesa" action="<?php echo BASE_URL; ?>payments.php" method="post" id="mpesa_form" target="_blank">
-                                        <!-- <input type="hidden" name="cmd" value="_xclick" /> -->
-                                        <!-- <input type="hidden" name="no_note" value="1" />
-                                        <input type="hidden" name="lc" value="UK" />
-                                        <input type="hidden" name="currency_code" value="USD" />
-                                        <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_LG.gif:NonHostedGuest" /> -->
-
-                                        <input type="hidden" name="final_total" value="<?php echo $final_total; ?>">
-                                        <div class="col-md-12 form-group">
-                                            <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_46; ?>" name="form1">
-                                        </div>
-                                    </form>
+                                
 
 
 
